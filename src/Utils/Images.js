1f732a4454463ea6d1b5730c38f3d81f84d52469
sohagbhuiyan <smsohag171@gmail.com>
2024-06-01 22:59:19 +0600

    myPortfolio

---
+++ src/Utils/Images.js
@@ -0,0 +1,21 @@
+import profile from "../assets/images/ff.png";
+import logo from "../assets/images/ee.png";
+
+import html from "../assets/images/html.png";
+import css from "../assets/images/css.png";
+import js from "../assets/images/js.png";
+import react from "../assets/images/re.png";
+import wd1 from "../assets/images/wd1.png";
+import wd2 from "../assets/images/wd2.png";
+import profile from "../assets/images/ff.png";
+import logo from "../assets/images/ee.png";
+import profile from "../assets/images/ff.png";
+import logo from "../assets/images/ee.png";
+
+
+const Images = {
+
+ 
+};
+
+export default Images;
